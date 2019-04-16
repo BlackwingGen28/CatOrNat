ee17c4ec71d550d65389b444d6571b30bbb56caf
Jordan Morgan <jordan.morgan2@smail.astate.edu>
2019-04-15 20:38:35 -0500

    incomplete project due to technical issues

--- CatOrNat/ViewController.swift
+++ CatOrNat/ViewController.swift
@@ -2,6 +2,8 @@
 //  ViewController.swift
 //  CatOrNat
 //
+//  could not get UIImageView variables or UILabel variables to work. This is an incomplete project.
+//
 //  Created by Jordan.Morgan2 on 4/15/19.
 //  Copyright © 2019 Jordan.Morgan2. All rights reserved.
 //
@@ -9,7 +11,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var catScore: Int = 0;//determine catness
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
